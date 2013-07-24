114f28084460c969441ba42e5c983a08ece1ec32
Andrew Gallant <jamslam@gmail.com>
2013-07-24 19:30:40 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+nflvid is a Python package that facilates the processing of NFL game footage. 
+In particular, this library provides routines to do the following:
+
+  - Download game footage from NFL's content provider (Neulion).
+
+  - Download play meta data associated with game footage that, among other 
+    things, describes the start time of every play in the game.
+
+  - Cut the game footage into pieces where each piece corresponds to a single 
+    game.
+
+  - Provide a simple command line interface to search plays and access the game 
+    footage for each play.
+
+The methods used in this library rely heavily on the open availability of data 
+that could be shut off at any time. More to the point, the content that this 
+library requires is large and cannot be distributed easily. Therefore, this 
+package's future remains uncertain.
+
+Installation, documentation and usage instructions are forthcoming.
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+import os
+
+longdesc = \
+'''A library to manage a relational database containing NFL data, including play-by-play statistics. The library also includes several scripts for maintaining and updating your database, including monitoring live games and keeping statistics up to date in near-realtime.'''
+
+try:
+    docfiles = map(lambda s: 'doc/%s' % s, list(os.walk('doc'))[0][2])
+except IndexError:
+    docfiles = []
+
+setup(
+    name='nfldb',
+    author='Andrew Gallant',
+    author_email='nfldb@burntsushi.net',
+    version='0.0.1',
+    license='WTFPL',
+    description='A library to manage and update NFL data in a relational '
+                'database.',
+    long_description=longdesc,
+    url='https://github.com/BurntSushi/nfldb',
+    classifiers=[
+        'License :: Public Domain',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Other Audience',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+    ],
+    platforms='ANY',
+    packages=['nfldb'],
+    package_dir={'nfldb': 'nfldb'},
+    # package_data={'nfldb': ['schedule-status', 'pbp-xml/*.xml.gz']}, 
+    data_files=[('share/doc/nfldb', ['README.md', 'COPYING', 'INSTALL']),
+                ('share/doc/nfldb/doc', docfiles)],
+    install_requires=['nflgame'],
+    scripts=[]
+)
