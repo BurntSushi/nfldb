5d9f896f927d035262ee3265bc6c8b046a802ef5
Andrew Gallant <jamslam@gmail.com>
2013-09-11 01:34:25 -0400

    Add the pytz dependency (added for nflgame anyway)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except LookupError:
         return {True: enc}.get(name == 'mbcs')
     codecs.register(wrapper)
 
-install_requires = ['nflgame', 'psycopg2', 'enum34']
+install_requires = ['nflgame', 'psycopg2', 'enum34', 'pytz']
 try:
     import argparse
 except ImportError:
